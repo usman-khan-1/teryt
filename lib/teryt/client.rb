af9e8c60c96727dd299ac83fc08c1ccee4136d37
Marcin Doliwa <mdoliwa@gmail.com>
2023-11-15 16:32:27 +0100

    Add PobierzListeGmin method

--- lib/teryt/client.rb
+++ lib/teryt/client.rb
@@ -26,12 +26,28 @@ module Teryt
         .map { |attrs| JednostkaTerytorialna.new(**attrs) }
     end
 
-    def pobierz_liste_powiatow(woj: "", data_stanu: Date.today)
+    def pobierz_liste_powiatow(woj:, data_stanu: Date.today)
       savon_client
         .call(:pobierz_liste_powiatow, message: { "tns:Woj" => woj, "tns:DataStanu" => data_stanu })
         .to_hash
         .dig(:pobierz_liste_powiatow_response, :pobierz_liste_powiatow_result, :jednostka_terytorialna)
         .map { |attrs| JednostkaTerytorialna.new(**attrs) }
+    end
+
+    def pobierz_liste_gmin(woj:, pow:, data_stanu: Date.today)
+      savon_client
+        .call(:pobierz_liste_gmin, message: { "tns:Woj" => woj, "tns:Pow" => pow, "tns:DataStanu" => data_stanu })
+        .to_hash
+        .dig(:pobierz_liste_gmin_response, :pobierz_liste_gmin_result, :jednostka_terytorialna)
+        .then { |result| Array.wrap(result) }
+        .map { |attrs| JednostkaTerytorialna.new(**attrs) }
+    end
+
+    def pobierz_slownik_rodzajow_jednostek
+      savon_client
+        .call(:pobierz_slownik_rodzajow_jednostek, message: {})
+        .to_hash
+        .dig(:pobierz_slownik_rodzajow_jednostek_response, :pobierz_slownik_rodzajow_jednostek_result, :string)
     end
   end
 end
